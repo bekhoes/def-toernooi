e07f67946baecfe89de4f8e3e985c30b218189c8
bekhoes <cas.bekhuis99@gmail.com>
2021-05-25 22:02:25 +0200

    style update

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -18,25 +18,25 @@
         <h1 class="lg:text-6xl sm:text-4xl font-bold def-header-class"><span class="text-red-500">DUTCH</span> <span class="text-white">ELITE</span> <span class="text-blue-600">FORCES</span></h1>
         <h1 class="lg:text-8xl sm:text-6xl font-bold text-black" >TOERNOOI</h1>
       </div>
-      <form class="form-bg p-4 rounded-xl" @submit.prevent="registerTeam">
+      <form class="form-bg p-4 rounded-xl" style="background-color: rgba(0, 0, 0, 0.50) !important;" @submit.prevent="registerTeam">
           <h1 class="text-center text-2xl font-bold text-white">REGISTRATIEFORMULIER</h1>
           <label class="text-white">
             Teamnaam
           </label>
-          <input type="text" class="form-input w-full opacity-100 text-white" placeholder="Teamnaam (Max. 20 karakters)" v-model="teamName">
+          <input type="text" class="form-input w-full opacity-100 text-white" style="background-color: transparent !important; border-color: white;" placeholder="Teamnaam (Max. 20 karakters)" v-model="teamName">
           <br><br><br>
           <label class="text-white">
             Discord Speler 1
           </label>
-          <input type="text" class="form-input w-full opacity-100 text-white" placeholder="Discord" v-model="teamMate1DSC">
+          <input type="text" class="form-input w-full opacity-100 text-white" style="background-color: transparent !important; border-color: white;" placeholder="Discord" v-model="teamMate1DSC">
           <label class="text-white">
             EFT Naam Speler 1
           </label>
-          <input type="text" class="form-input w-full opacity-100 text-white" placeholder="EFT" v-model="teamMate1BSG">
+          <input type="text" class="form-input w-full opacity-100 text-white" style="background-color: transparent !important; border-color: white;" placeholder="EFT" v-model="teamMate1BSG">
           <label class="text-white">
             Wapenklasse Speler 1
           </label>
-          <select class="form-select w-full text-white" v-model="teamMate1Class">
+          <select class="form-select w-full text-white" v-model="teamMate1Class" style="background-color: transparent !important; border-color: white;">
             <option style="color: black;" value="AR" :disabled="teamMate3Class == 'AR' || teamMate2Class == 'AR'">AR</option>
             <option style="color: black;" value="DMR" :disabled="teamMate3Class == 'DMR' || teamMate2Class == 'DMR'">DMR</option>
             <option style="color: black;" value="SMG" :disabled="teamMate3Class == 'SMG' || teamMate2Class == 'SMG'">SMG</option>
@@ -45,15 +45,15 @@
           <label class="text-white">
             Discord Speler 2
           </label>
-          <input type="text" class="form-input w-full opacity-100 text-white" placeholder="Discord" v-model="teamMate2DSC">
+          <input type="text" class="form-input w-full opacity-100 text-white" style="background-color: transparent !important; border-color: white;" placeholder="Discord" v-model="teamMate2DSC">
           <label class="text-white">
             EFT Naam Speler 2
           </label>
-          <input type="text" class="form-input w-full opacity-100 text-white" placeholder="EFT" v-model="teamMate2BSG">
+          <input type="text" class="form-input w-full opacity-100 text-white" style="background-color: transparent !important; border-color: white;" placeholder="EFT" v-model="teamMate2BSG">
           <label class="text-white">
             Wapenklasse Speler 2
           </label>
-          <select class="form-select w-full text-white" v-model="teamMate2Class">
+          <select class="form-select w-full text-white" v-model="teamMate2Class" style="background-color: transparent !important; border-color: white;">
             <option style="color: black;" value="AR" :disabled="teamMate3Class == 'AR' || teamMate1Class == 'AR'">AR</option>
             <option style="color: black;" value="DMR" :disabled="teamMate3Class == 'DMR' || teamMate1Class == 'DMR'">DMR</option>
             <option style="color: black;" value="SMG" :disabled="teamMate3Class == 'SMG' || teamMate1Class == 'SMG'">SMG</option>
@@ -62,32 +62,32 @@
            <label class="text-white">
             Discord Speler 3
           </label>
-          <input type="text" class="form-input w-full opacity-100 text-white" placeholder="Discord" v-model="teamMate3DSC">
+          <input type="text" class="form-input w-full opacity-100 text-white" style="background-color: transparent !important; border-color: white;" placeholder="Discord" v-model="teamMate3DSC">
           <label class="text-white">
             EFT Naam Speler 3
           </label>
-          <input type="text" class="form-input w-full opacity-100 text-white" placeholder="EFT" v-model="teamMate3BSG">
+          <input type="text" class="form-input w-full opacity-100 text-white" style="background-color: transparent !important; border-color: white;" placeholder="EFT" v-model="teamMate3BSG">
           <label class="text-white">
             Wapenklasse Speler 3
           </label>
-          <select class="form-select w-full text-white" v-model="teamMate3Class">
+          <select class="form-select w-full text-white" v-model="teamMate3Class" style="background-color: transparent !important; border-color: white;">
             <option style="color: black;" value="AR" :disabled="teamMate2Class == 'AR' || teamMate1Class == 'AR'">AR</option>
             <option style="color: black;" value="DMR" :disabled="teamMate2Class == 'DMR' || teamMate1Class == 'DMR'">DMR</option>
             <option style="color: black;" value="SMG" :disabled="teamMate2Class == 'SMG' || teamMate1Class == 'SMG'">SMG</option>
           </select>
           <br><br>
-          <button class="def-button text-white" @submit.prevent="registerTeam">Registreer</button>
+          <button class="def-button text-white" @submit.prevent="registerTeam" style="border: 2px solid; border-radius: 15px; padding: 8px 32px; font-weight: 900;">Registreer</button>
       </form>
     </div>
     <div class="mx-auto w-1/2" v-if="started == 'true'">
-      <div class="text-center mt-8 pt-20">**
+      <div class="text-center mt-8 pt-20">
         <h1 class="lg:text-6xl sm:text-4xl font-bold def-header-class"><span class="text-red-500">DUTCH</span> <span class="text-white">ELITE</span> <span class="text-blue-600">FORCES</span></h1>
         <h1 class="lg:text-8xl sm:text-6xl font-bold text-black" >TOERNOOI</h1>
         <h1 class="text-4xl font-bold text-white">LIVE SCOREBORD</h1>
       </div>
       <div class="text-6xl mb-4">
       </div>
-      <table class="table table-auto w-full text-left rounded-xl def-table">
+      <table class="table table-auto w-full text-left rounded-xl def-table text-white" style="background-color: rgba(0, 0, 0, 0.5)">
         <thead>
           <tr>
             <th class="p-2">#</th>
@@ -111,7 +111,7 @@
       </table>
     </div>
     <div class="mx-auto w-1/2" v-if="started == 'hold'">
-      <div class="text-center mt-8 pt-20">**
+      <div class="text-center mt-8 pt-20">
         <h1 class="lg:text-6xl sm:text-4xl font-bold def-header-class"><span class="text-red-500">DUTCH</span> <span class="text-white">ELITE</span> <span class="text-blue-600">FORCES</span></h1>
         <h1 class="lg:text-8xl sm:text-6xl font-bold text-black" >TOERNOOI</h1>
         <h1 class="text-4xl font-bold text-white">AANMELDINGEN GESLOTEN!</h1>
@@ -247,7 +247,7 @@ body > div#app {
   background-color: #0A0809 !important;
 }
 .form-bg {
-  background-color: rgba(0, 0, 0, 0.50)
+  
 }
 .text-red-500 {
   color: #b5223d !important;
